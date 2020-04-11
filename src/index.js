9c05c7e2808c91ba6d8a032380a0614f26b3e098
Finn Evermann <finnevermann@gmx.de>
2020-04-11 02:19:40 +0200

    switched to browser router

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
