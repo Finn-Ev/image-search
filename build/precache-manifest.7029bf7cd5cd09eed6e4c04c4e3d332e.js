91a2b2b5239ff24b37f9b32de7861fd2cbae5cea
Finn Evermann <finnevermann@gmx.de>
2020-06-21 15:34:47 +0200

    build directory

---
+++ build/precache-manifest.7029bf7cd5cd09eed6e4c04c4e3d332e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ebdaf859bfedef20e209c08d380924",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f3dc4d79a8215691bbb",
+    "url": "/static/css/main.edb85cc3.chunk.css"
+  },
+  {
+    "revision": "945c71758b2e7d9e1f6f",
+    "url": "/static/js/2.e1e9ee16.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/static/js/2.e1e9ee16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f3dc4d79a8215691bbb",
+    "url": "/static/js/main.75829c35.chunk.js"
+  },
+  {
+    "revision": "be6efebfcec26383762c",
+    "url": "/static/js/runtime-main.bddec228.js"
+  },
+  {
+    "revision": "ea535c060af7b370227fb83c126ae748",
+    "url": "/static/media/form-background.ea535c06.png"
+  },
+  {
+    "revision": "e7fd4e370acdd6d6b7f3fedbf7b506bd",
+    "url": "/static/media/info-page-background.e7fd4e37.png"
+  }
+]);
